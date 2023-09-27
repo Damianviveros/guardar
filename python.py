79fecb875fd0ebe86edd21fa032a59fbc0824649
Damian Alvarez <damianalva20044@gmail.com>
2023-09-27 16:48:55 -0600

    nuevo

--- python.py
+++ python.py
@@ -1,18 +1,3 @@
 import math
-n=int(input("HELLO"))
-deltx=(2-0)/n
-h=deltx/2
-print(h)
-list=[]
-x=0
-for i in range(n):
-    x=(x+deltx)
-    list.append(math.sin(pow(x,2)))
-print(list)
-suma=0
-suma=sum(list)
-inter=h*suma
-print(suma)
-print(inter)
-print(math.sin(2))
-8
+deltx=((2-0)/8)*5
+print(deltx)
